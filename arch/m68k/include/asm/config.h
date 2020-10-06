ec8b81724d7b485cf776cc5c0d0cc12b6d218b2c
Laurent Vivier <laurent@vivier.eu>
2020-10-06 18:41:05 +0200

    m68k: introduce a virtual m68k machine
    This machine allows to have up to 3.2 GiB and 128 Virtio devices.
    
    It is based on android goldfish devices.
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/asm/config.h
+++ arch/m68k/include/asm/config.h
@@ -17,6 +17,7 @@ extern int mvme16x_parse_bootinfo(const struct bi_record *record);
 extern int mvme147_parse_bootinfo(const struct bi_record *record);
 extern int hp300_parse_bootinfo(const struct bi_record *record);
 extern int apollo_parse_bootinfo(const struct bi_record *record);
+extern int virt_parse_bootinfo(const struct bi_record *record);
 
 extern void config_amiga(void);
 extern void config_atari(void);
@@ -29,5 +30,6 @@ extern void config_bvme6000(void);
 extern void config_hp300(void);
 extern void config_q40(void);
 extern void config_sun3x(void);
+extern void config_virt(void);
 
 #endif /* _M68K_CONFIG_H */
